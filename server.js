ae53e934c3ede2558b5048c166648f5032a06149
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-01-14 11:15:35 -0800

    Added dummy data to friends.js and added html to home.html

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+
+const apiRoutes = require("./app/routing/apiRoutes");
+const htmlRoutes = require("./app/routing/htmlRoutes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
