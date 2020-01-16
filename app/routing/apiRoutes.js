1240c7336d2a0a7c658ef649035c299815f5fa58
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-01-16 10:05:59 -0800

    Added choices array in choices.js; added a route for choices;

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,11 @@
 var friendData = require("../data/friends");
+var choicesData = require("../data/choices");
 
 module.exports = function (app) {
+    app.get("/api/choices/:type", function (req, res) {
+        return (req.params.type == 2) ? res.send(aboutYouArray) : res.send(choicesArray);
+    });
+
     app.get("/api/friends", function (req, res) {
         res.json(friendData);
     });
