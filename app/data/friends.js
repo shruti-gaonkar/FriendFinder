4c73c16b81dd97550ee3cc05457d9e959fdbee37
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-01-17 10:39:07 -0800

    Post data of the survey form and added logic to get the difference between the user and friends score

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,22 @@ var friendsArray = [
             "4",
             "1"
         ]
+    },
+    {
+        "name": "Will",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+            "5",
+            "3",
+            "4",
+            "4",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1"
+        ]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,7 +12,26 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         let newFriend = req.body;
-        //friendData.push(newFriend);
-        //res.json(true);
+        //console.log(newFriend);
+        let resultArr = [];
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        for (var i = 0; i < friendData.length; i++) {
+            const frdArr = friendData[i].scores;
+            //console.log(frdArr);
+            /*frdArr.forEach(function (val, index) {
+                let diff = val - newFriend.scores[index];
+            });*/
+            const diffArr = frdArr.map((val, index) => Math.abs(val - newFriend.scores[index]));
+
+            const diff = diffArr.reduce(reducer);
+            //console.log(diff);
+            resultArr.push({
+                name: friendData[i].name,
+                photo: friendData[i].photo,
+                diff: diff
+            });
+        }
+
+        console.log(resultArr);
     });
 }
