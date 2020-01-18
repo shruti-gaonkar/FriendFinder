1948d98bf0f68b55748f4120e5220b6c931b330d
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2020-01-18 00:13:40 -0800

    Implemented the functionality to write profile data entered to friends.js once the match is found

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,6 @@
-var friendData = require("../data/friends");
-var choicesData = require("../data/choices");
+let friendData = require("../data/friends");
+const choicesData = require("../data/choices");
+const fs = require("fs");
 
 module.exports = function (app) {
     app.get("/api/choices", function (req, res) {
@@ -12,7 +13,6 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         let newFriend = req.body;
-        //console.log(newFriend);
         let resultArr = [];
         const reducer = (accumulator, currentValue) => accumulator + currentValue;
         for (var i = 0; i < friendData.length; i++) {
@@ -24,7 +24,7 @@ module.exports = function (app) {
             const diffArr = frdArr.map((val, index) => Math.abs(val - newFriend.scores[index]));
 
             const diff = diffArr.reduce(reducer);
-            //console.log(diff);
+
             resultArr.push({
                 name: friendData[i].name,
                 photo: friendData[i].photo,
@@ -37,6 +37,23 @@ module.exports = function (app) {
         const retVal = Math.min.apply(Math, resultArr.map(function (o) { return o.diff; }))
 
         const matchedFriend = resultArr.find(function (o) { return o.diff == retVal; })
+
+        friendData.push(newFriend);
+
+        let friendStr = 'var friendsArray =';
+        friendStr += JSON.stringify(friendData);
+        friendStr += '\nmodule.exports = friendsArray;';
+        fs.writeFile("./app/data/friends.js", friendStr, function (err) {
+
+            // If the code experiences any errors it will log the error to the console.
+            if (err) {
+                return console.log(err);
+            }
+
+            // Otherwise, it will print: "movies.txt was updated!"
+            console.log("Friends list is updated!");
+
+        });
 
         res.json(matchedFriend);
     });
